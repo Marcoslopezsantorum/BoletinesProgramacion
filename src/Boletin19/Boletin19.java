669320111f3dee59d1b477fcd7bd8f72c28d2ad7
Marcoslopezsantorum <mlopezsantorum@danielcastelao.org>
2021-04-15 13:35:08 +0200

    Boletin19

---
+++ src/Boletin19/Boletin19.java
@@ -0,0 +1,21 @@
+package Boletin19;
+
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+
+public class Boletin19 {
+
+
+    public static void main(String[] args) {
+
+        Ventana obx = new Ventana();
+        obx.iniciar();
+
+    }
+}
