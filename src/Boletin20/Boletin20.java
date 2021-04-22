7d08e94e432a47f691e9166ea8aebe5cc752d2e8
Marcoslopezsantorum <mlopezsantorum@danielcastelao.org>
2021-04-22 21:30:18 +0200

    Boletin20

---
+++ src/Boletin20/Boletin20.java
@@ -0,0 +1,15 @@
+package Boletin20;
+
+
+import com.marcos.Ventanaa.Ventana;
+
+public class Boletin20 {
+
+
+    public static void main(String[] args) {
+
+        Ventana obx = new Ventana();
+        obx.iniciar();
+
+    }
+}
