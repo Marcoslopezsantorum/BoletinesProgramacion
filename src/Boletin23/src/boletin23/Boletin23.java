d492208d6533733ea674d3f197c6092001698fe9
Marcoslopezsantorum <72596886+Marcoslopezsantorum@users.noreply.github.com>
2021-05-03 17:52:44 +0200

    Add files via upload

---
+++ src/Boletin23/src/boletin23/Boletin23.java
@@ -0,0 +1,20 @@
+
+package boletin23;
+
+import com.Marcos.calculadora.Calculadora;
+
+
+public class Boletin23 {
+
+  
+    public static void main(String[] args) {
+      
+       Calculadora obx = new Calculadora();
+           
+       obx.setVisible(true);
+           
+       
+               
+    }
+    
+}
