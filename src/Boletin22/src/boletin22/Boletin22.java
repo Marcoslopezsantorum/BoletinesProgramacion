8540acb9afe50a659cb49cdc9fa238d367a1f947
Marcoslopezsantorum <72596886+Marcoslopezsantorum@users.noreply.github.com>
2021-05-03 19:05:42 +0200

    Add files via upload

---
+++ src/Boletin22/src/boletin22/Boletin22.java
@@ -0,0 +1,16 @@
+
+package boletin22;
+
+import com.Marcos.Ventana.Ventana;
+
+
+public class Boletin22 {
+
+
+    public static void main(String[] args) {
+    Ventana obx = new Ventana();
+    obx.setVisible(true);
+      
+            }
+    
+}
